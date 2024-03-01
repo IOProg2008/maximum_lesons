44d4b3b189568cad0168783fb835ee2adfc7752e
IOProg2008 <122290039+IOProg2008@users.noreply.github.com>
2024-03-01 18:46:08 +0400

    2th

---
+++ m1/list.py
@@ -0,0 +1,16 @@
+
+# num=123
+# mylist=[1,2,'five',num]
+#
+# # print(mylist[-2])
+#
+#
+# # print(mylist)
+#
+# # text='12345
+# # text_list=list(text)
+# # print(text_list)
+#
+# for i in mylist:
+#     print(i)
+# print('The end')
