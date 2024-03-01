b433778efd7cd600a38a550b205d7452f976b8f8
IOProg2008 <122290039+IOProg2008@users.noreply.github.com>
2024-03-01 18:43:19 +0400

    1th

---
+++ m1/clas2.py
@@ -0,0 +1,11 @@
+login="bob"
+pasword="qwerty"
+
+login_inpot = input()
+pasword_inpot = input()
+
+
+if login != login_inpot or pasword != pasword_inpot:
+    print('Доступ закрыт')
+else:
+    print('welcome')
---
+++ m1/guui.py
@@ -0,0 +1,5 @@
+for num in range(20,200):
+    a=num/2
+    if type(a)!='float':
+        print(num)
+
