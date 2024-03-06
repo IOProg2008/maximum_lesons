44eecce86150488899d84e097fed07bff52b9490
IOProg2008 <122290039+IOProg2008@users.noreply.github.com>
2024-03-06 18:08:12 +0400

    8th

--- m1/strings/les.py
+++ m1/strings/les.py
@@ -1,3 +1,36 @@
 # строки
-string="string"
-print(string)
+string='string'
+string_2="строка"
+# print(string)
+
+# конкатинация
+# print(string+string_2)
+
+# строка-не изменяемый тип данных
+
+# string+"123"
+# print(string)
+# не изменение,а пересохранение
+
+# форматирование
+
+
+# for i in range(10):
+#     print(f'i = {i}')
+
+# разбитие
+# string_s="s t r i n g"
+# string_u='s/t/r/i/n/g'
+# print(string.split())
+# print(string_s.split())
+# print(string_u.split('/'))
+
+# замена симвалов
+# i='011100000221545010202522052202202'
+#
+# print(i.replace("0","a"))
+
+# строка из списка
+# i=['Hi','Bro']
+# o=','.join(i)
+# print(o)
